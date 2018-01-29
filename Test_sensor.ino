0e3f620df77fbf02ba0b2e9abd4bfaf4c676e278
DavidCastilloAlvarado <dcastilloa@uni.pe>
2018-01-28 22:01:37 -0500

    Add files via upload

---
+++ Test_sensor.ino
@@ -0,0 +1,24 @@
+/*
+Autor: Castillo Alvarado , David
+test: Sensor
+*/
+
+int sensorPin = A0;    // select the input pin for the potentiometer
+double ang=0;
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup() {
+  // declare the ledPin as an OUTPUT:
+
+  Serial.begin(9600);           // set up Serial library at 9600 bps
+}
+
+void loop() {
+  // read the value from the sensor:
+  sensorValue = analogRead(sensorPin);
+  ang=-0.1899*sensorValue + 102.18;
+  // A 90 grados mide 116bits , a 0 grados mide 433 bits
+ Serial.print( ang ); Serial.println( '\t' ); //Serial.println( ang);
+  // turn the ledPin on
+
+}
